2dcded6f3db32687acf0eff6242a0df9dd4b0555
vrugtehagel <vrugtehagel@pm.me>
2024-02-08 16:18:59 +0900

    Move PHP-in-CSS to a CSS file

---
+++ solution.css.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-type: text/css; charset: UTF-8");
+
+// Okay, this might look like I'm cheating, but I promise I'm not!
+// Basically, CSS is not capable of generating any form of "random" numbers.
+// So I can just hardcode a solution in CSS, but then the game is only fun once.
+// I thought that would be a bit lame.
+// So this PHP snippet outputs some CSS with a unique solution in the form
+// of some CSS variables.
+
+// So the game is still 100% CSS :)
+
+$solution = array_rand(range(0, 5), 4);
+shuffle($solution);
+
+echo ":root {";
+foreach($solution as $index => $color)
+	echo "--s$index: $color;";
+echo "}";
+
+?>
